9b798dbc940cfa98fd36299b5cfc7a43f451a5db
Yixuan D ang <ydang@e1z1r2p15.42.us.org>
2016-10-21 05:10:53 -0700

    ft_list_foreach is passed the test

---
+++ level4/ft_list_foreach/ft_list_foreach.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/21 05:06:02 by ydang             #+#    #+#             */
+/*   Updated: 2016/10/21 05:08:22 by ydang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	t_list	*list_ptr;
+
+	list_ptr = begin_list;
+	while (list_ptr)
+	{
+		(*f)(list_ptr->data);
+		if (!(list_ptr->next))
+			break;
+		list_ptr = list_ptr->next;
+	}
+}
