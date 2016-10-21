9b95d11bccc2df59c3ddd247cafa16ab1d5fa502
Yixuan D ang <ydang@e1z1r2p15.42.us.org>
2016-10-21 05:26:46 -0700

    sort_int_tab passed the test

---
+++ level4/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_int_tab.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/21 05:22:53 by ydang             #+#    #+#             */
+/*   Updated: 2016/10/21 05:25:51 by ydang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	int				tmp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				tmp = 0;
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
