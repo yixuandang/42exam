f3f7b091791b6b4a79520d77f476510e55547b9f
Yixuan D ang <ydang@e1z2r8p20.42.us.org>
2016-10-22 23:48:21 -0700

    should be pass the test

---
+++ level4/ft_list_remove_if/ft_list_remove_if.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/16 01:03:44 by ydang             #+#    #+#             */
+/*   Updated: 2016/10/20 05:30:46 by ydang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stdlib.h>
+
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	t_list	*tmp;
+	t_list	*i;
+
+	while (*begin_list && cmp((*begin_list)->data, data_ref) == 0)
+	{
+		tmp = *begin_list;
+		*begin_list = (*begin_list)->next;
+		free(tmp);
+	}
+	i = *begin_list;
+	while (i && i->next)
+	{
+		if (cmp(i->next->data, data_ref) == 0)
+		{
+			tmp = i->next;
+			i->next = tmp->next;
+			free(tmp);
+		}
+		if (i->next)
+		{
+			i = i->next;
+		}
+	}
+}
